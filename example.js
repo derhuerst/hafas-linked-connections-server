037ec861ba5de702c85c824ab7761e36d185353f
Jannis R <mail@jannisr.de>
2020-04-24 16:08:35 +0200

    example: caching

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const createClient = require('vbb-hafas')
+const sqlite3 = require('sqlite3')
+const sqliteStore = require('cached-hafas-client/stores/sqlite')
+const withCaching = require('cached-hafas-client')
 const createServer = require('.')
 
 const BASE_URL = 'http://localhost:3000/'
@@ -11,7 +14,10 @@ const BBOX = {
 	east: 13.43
 }
 
-const client = createClient('hafas-linked-connections-server')
+const hafas = createClient('hafas-linked-connections-server')
+
+const db = new sqlite3.Database('example-cache.sqlite')
+const client = withCaching(hafas, sqliteStore(db))
 
 const server = createServer(BASE_URL, client, BBOX)
 server.listen(3000)
